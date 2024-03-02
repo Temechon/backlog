ae31ee1af334d84e81a8987750eed10a58bbe69a
Julian <julian.chenard@gmail.com>
2024-03-02 15:27:59 +0100

    style updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'backlogg';
+}
+
+
+export function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
